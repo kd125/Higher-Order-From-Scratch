8f58e0df0bf81f6ac9c09f0d9e74ff840be0ad2c
Kd125 <kashdangar1250@gmail.com>
2023-02-11 15:32:36 -0600

    new commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// .map()
+
+const myArray = [2, 5, 17, 37, 8, 12, 24];
+
+// let usingMap = myArray.map((x) => x * 2);
+// console.log(usingMap);
+
+const multiplyByTwo = (arrInput) => {
+  let blankArray = [];
+  for (let i = 0; i < arrInput.length; i++) {
+    let newNumber = arrInput[i] * 2;
+    blankArray.push(newNumber);
+  }
+  console.log(blankArray);
+};
+
+// console.log(multiplyByTwo(myArray));
+
+// *************************************************************************************************
+
+//.reduce()
+
+const addingValues = (arr1) => {
+  let totalValue = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    totalValue = totalValue + arr1[i];
+  }
+  console.log(totalValue);
+};
+
+// console.log(addingValues(myArray));
+
+// *************************************************************************************************
+
+//.filter()
+const filteredArray = (arr1) => {
+  let newArray = [];
+  for (let i = 0; i < arr1.length; i++) {}
+};
